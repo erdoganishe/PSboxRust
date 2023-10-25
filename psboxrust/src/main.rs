40648d72c4215897214f9d9608dfe1fbd4bde719
erdoganishe <khotyanmisha@gmail.com>
2023-10-25 14:56:10 +0300

    .md edited

--- psboxrust/src/main.rs
+++ psboxrust/src/main.rs
@@ -4,24 +4,24 @@ use s_box::{generate_s_table, generate_inverse_s_table, s_box_encrypt, s_box_dec
 use p_box::{p_block, p_block_decrypt};
 
 fn main() {
-    let p_table: [u8; 32] = [7, 14, 22, 1, 0, 6, 20, 16, 27, 15, 12, 10, 8, 23, 28, 5, 29, 2, 9, 26, 19, 25, 11, 18, 4, 13, 17, 21, 3, 24, 30, 31];
+    let p_table: [u8; 8] = [3, 5, 2, 1, 0, 6, 7, 4];
 
-    let input_data: u32 = 0b11001101010111010001110010110100;
+    let input_data: u8 = 0b11001101;
 
     let s_table = generate_s_table();
     let encrypted_data_s_box = s_box_encrypt(input_data, &s_table);
 
     let permuted_data = p_block(encrypted_data_s_box, &p_table);
 
-    println!("Вхідні дані: {:032b}", input_data);
-    println!("Зашифровані дані (S-блок): {:032b}", encrypted_data_s_box);
-    println!("Після P-блоку: {:032b}", permuted_data);
+    println!("Вхідні дані: {:08b}", input_data);
+    println!("Зашифровані дані (S-блок): {:08b}", encrypted_data_s_box);
+    println!("Після P-блоку: {:08b}", permuted_data);
 
     let decrypted_data_p_block = p_block_decrypt(permuted_data, &p_table);
 
-    println!("Розшифровані дані (P-блок): {:032b}", decrypted_data_p_block);
+    println!("Розшифровані дані (P-блок): {:08b}", decrypted_data_p_block);
 
     let decrypted_data_s_block = s_box_encrypt(decrypted_data_p_block, &s_table);
 
-    println!("Розшифровані дані (S-блок): {:032b}", decrypted_data_s_block);
+    println!("Розшифровані дані (S-блок): {:08b}", decrypted_data_s_block);
 }
--- psboxrust/src/p_box.rs
+++ psboxrust/src/p_box.rs
@@ -1,7 +1,8 @@
-pub fn p_block(input: u32, p_table: &[u8; 32]) -> u32 {
-    let mut result: u32 = 0;
 
-    for i in 0..32 {
+pub fn p_block(input: u8, p_table: &[u8; 8]) -> u8 {
+    let mut result: u8 = 0;
+
+    for i in 0..8 {
         let bit = (input >> p_table[i]) & 1;
         result |= bit << i;
     }
@@ -9,16 +10,17 @@ pub fn p_block(input: u32, p_table: &[u8; 32]) -> u32 {
     result
 }
 
-pub fn p_block_decrypt(input: u32, p_table: &[u8; 32]) -> u32 {
-    let mut reverse_table: [u8; 32] = [0; 32];
 
-    for i in 0..32 {
+pub fn p_block_decrypt(input: u8, p_table: &[u8; 8]) -> u8 {
+    let mut reverse_table: [u8; 8] = [0; 8];
+
+    for i in 0..8 {
         reverse_table[p_table[i] as usize] = i as u8;
     }
 
-    let mut result: u32 = 0;
+    let mut result: u8 = 0;
 
-    for i in 0..32 {
+    for i in 0..8 {
         let bit = (input >> reverse_table[i]) & 1;
         result |= bit << i;
     }

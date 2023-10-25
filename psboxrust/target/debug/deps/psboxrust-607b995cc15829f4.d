d4c71cd21c265c24b6a8b327feb2af0cf7b663cd
erdoganishe <khotyanmisha@gmail.com>
2023-10-25 12:15:55 +0300

    project created

---
+++ psboxrust/target/debug/deps/psboxrust-607b995cc15829f4.d
@@ -0,0 +1,5 @@
+C:\Users\khoty\Documents\Projects\PSboxRust\psboxrust\target\debug\deps\psboxrust-607b995cc15829f4.rmeta: src\main.rs
+
+C:\Users\khoty\Documents\Projects\PSboxRust\psboxrust\target\debug\deps\psboxrust-607b995cc15829f4.d: src\main.rs
+
+src\main.rs:
---
+++ psboxrust/target/debug/deps/psboxrust-d68c6541e675bb02.d
@@ -0,0 +1,5 @@
+C:\Users\khoty\Documents\Projects\PSboxRust\psboxrust\target\debug\deps\psboxrust-d68c6541e675bb02.rmeta: src\main.rs
+
+C:\Users\khoty\Documents\Projects\PSboxRust\psboxrust\target\debug\deps\psboxrust-d68c6541e675bb02.d: src\main.rs
+
+src\main.rs:
